ec2b728f9180417e685d3f389b2aedc4c1793a0a
Mattias Rådemar <mattiasrademar@gmail.com>
2019-11-30 13:13:59 +0100

    first commit to new repo.

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+
+type Props = {
+  props: any
+}
+
+export default props => {
+  return (
+    <>
+      <h1>Hey from gatsby</h1>
+    </>
+  )
+}
