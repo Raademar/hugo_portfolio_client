8fbb4726742a5ab25fdc35540ff812c73b2b7e12
Mattias Rådemar <mattiasrademar@gmail.com>
2020-01-18 12:42:56 +0100

    some more setup files.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react'
+import Header from '../components/Header/Header'
 
 type Props = {
   props: any
@@ -7,6 +8,7 @@ type Props = {
 export default props => {
   return (
     <>
+      <Header />
       <h1>Hey from gatsby</h1>
     </>
   )
