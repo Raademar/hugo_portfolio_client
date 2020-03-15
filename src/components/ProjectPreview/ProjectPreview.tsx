105a7db20691c5d7c51863c29a512dd2f19b3dc9
Mattias Rådemar <mattiasrademar@gmail.com>
2020-03-15 13:41:25 +0100

    updated vimeo src

--- src/components/ProjectPreview/ProjectPreview.tsx
+++ src/components/ProjectPreview/ProjectPreview.tsx
@@ -5,67 +5,77 @@ import Img from 'gatsby-image'
 import styles from './ProjectPreview.module.scss'
 
 type Props = {
-	featuredProject: boolean
-	image: any
-	title: string
-	url: string
-	left?: boolean
-	right?: boolean
-	gridRow?: number
+  featuredProject: boolean
+  image: any
+  title: string
+  url: string
+  vimeoSrc: string
+  left?: boolean
+  right?: boolean
+  gridRow?: number
 }
 
 export const ProjectPreview: FunctionComponent<Props> = props => {
-	const { featuredProject, image, title, url, left, right, gridRow } = props
-	const [isHovered, setIsHovered] = useState(false)
-	const [isLoaded, setIsLoaded] = useState(false)
-	const [isPlaying, setIsPlaying] = useState(false)
-	const imageRef = useRef(null)
+  const {
+    featuredProject,
+    image,
+    title,
+    url,
+    left,
+    right,
+    gridRow,
+    vimeoSrc
+  } = props
+  const [isHovered, setIsHovered] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const imageRef = useRef(null)
 
-	return (
-		<article
-			className={`${
-				featuredProject
-					? styles.projectPreviewContainerFull
-					: styles.projectPreviewContainer
-			}`}
-		>
-			<div
-				className={styles.projectImageContainer}
-				onMouseEnter={() => {
-					setIsPlaying(true)
-					setIsHovered(true)
-				}}
-				onMouseLeave={() => {
-					setIsHovered(false)
-					setIsPlaying(false)
-				}}
-			>
-				<Link to={url}>
-					{/* {isHovered ? ( */}
-					<div className={styles.playerWrapper}>
-						<ReactPlayer
-							url='https://vimeo.com/310379413'
-							className={styles.reactPlayer}
-							playing={isPlaying && isLoaded}
-							controls={false}
-							width='100%'
-							height='100%'
-							onReady={() => {
-								setIsLoaded(true)
-							}}
-							volume={0}
-							muted
-							preload
-						/>
-					</div>
-					{/* ) : (
+  return (
+    <article
+      className={`${
+        featuredProject
+          ? styles.projectPreviewContainerFull
+          : styles.projectPreviewContainer
+      }`}
+    >
+      <div
+        className={styles.projectImageContainer}
+        onMouseEnter={() => {
+          setIsPlaying(true)
+          setIsHovered(true)
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false)
+          setIsPlaying(false)
+        }}
+      >
+        <Link to={url}>
+          {/* {isHovered ? ( */}
+          <div className={styles.playerWrapper}>
+            <ReactPlayer
+              url={vimeoSrc}
+              className={styles.reactPlayer}
+              playing={isPlaying && isLoaded}
+              controls={false}
+              width='100%'
+              height='100%'
+              onReady={() => {
+                setIsLoaded(true)
+              }}
+              volume={0}
+              muted
+              preload
+            />
+          </div>
+          {/* ) : (
 					 	<Img fluid={image} alt={title} ref={imageRef} />
 					 )} */}
-				</Link>
-			</div>
-			<div className={styles.linkContainer}>
-				<h3>{title}</h3>
-			</div>
-		</article>
-	)
+        </Link>
+      </div>
+      <div className={styles.linkContainer}>
+        <h3>{title}</h3>
+      </div>
+    </article>
+  )
 }
