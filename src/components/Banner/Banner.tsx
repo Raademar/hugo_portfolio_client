0d8993734a6923beaf48e85120de7fa8d191edcd
Mattias Rådemar <mattiasrademar@gmail.com>
2020-03-16 16:25:48 +0100

    flickity

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent, useRef, useEffect } from 'react'
+import React, { FunctionComponent, useRef, useEffect, useState } from 'react'
 import { useStaticQuery, graphql, Link, navigate } from 'gatsby'
 import Img from 'gatsby-image'
 import ImageGallery from 'react-image-gallery'
+import Flickity from 'react-flickity-component'
 import styles from './Banner.module.scss'
 import { useWindowSize } from '../../helpers/helpers'
 
@@ -29,7 +30,10 @@ export const Banner: FunctionComponent<Props> = (props: Props) => {
 			}
 		}
 	`)
+	const [cords, setCords] = useState({ x: 0, y: 0 })
 	const imageRef = useRef(null)
+	let down: any
+	let timeTaken = 0
 	const bannerProjects = data.allSanityProject.nodes
 		.filter((node: any) => node.featuredProject)
 		.map((item: any, index: number) => {
@@ -40,17 +44,43 @@ export const Banner: FunctionComponent<Props> = (props: Props) => {
 			}
 		})
 
-	const navigateToImageUrl = (index: number) => {
+	const navigateToImageUrl = (e: any, index: number) => {
+		// setCords({ x: e.screenX, y: e.screenY })
+		e.stopPropogation()
+		console.log(e.screenX, e.screenY)
 		const url: string = bannerProjects[index].url
 		navigate(url)
+	}
+
+	const mousePress = () => {
+		console.log('down')
+
+		down = Date.now()
+	}
+
+	const mouseReleased = (index: number) => {
+		console.log('up')
+
+		timeTaken = Date.now() - down
+		console.log(timeTaken)
+		// navigateToImageUrl(index)
 	}
 
 	const size = useWindowSize()
 	const IS_MOBILE = size && size.width < 767
 
+	const flickityOptions = {
+		initialIndex: bannerProjects.length,
+		prevNextButtons: false,
+		pageDots: false,
+		wrapAround: true,
+		cellAlign: 'center'
+		// adaptiveHeight: true,
+	}
+
 	return (
 		<div className={styles.container}>
-			<ImageGallery
+			{/* <ImageGallery
 				items={bannerProjects}
 				showThumbnails={false}
 				showFullscreenButton={false}
@@ -62,7 +92,25 @@ export const Banner: FunctionComponent<Props> = (props: Props) => {
 				}
 				ref={imageRef}
 				// getCurrentIndex
-			/>
+			/> */}
+			<Flickity
+				className={'carousel'}
+				elementType={'div'}
+				options={flickityOptions}
+				flickityRef={c => imageRef}
+			>
+				{bannerProjects.map((image: any, index: number) => {
+					return (
+						<img
+							src={image.original}
+							// onMouseDown={() => console.log('down')}
+							// onMouseUp={() => mouseReleased(index)}
+							key={index}
+							onClick={e => navigateToImageUrl(e, index)}
+						/>
+					)
+				})}
+			</Flickity>
 		</div>
 	)
 }
