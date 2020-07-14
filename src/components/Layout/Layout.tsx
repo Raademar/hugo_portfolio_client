59e2af9df22b7acfbb50c294488123e3548723d3
Mattias <mattiasrademar@gmail.com>
2020-07-14 22:00:25 +0200

    removed tacky water mark for creators, changed meta text.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,82 +1,82 @@
-import { graphql, StaticQuery } from 'gatsby'
-import React, { useState } from 'react'
-import Headroom from 'react-headroom'
-import Helmet from 'react-helmet'
-import { Header } from '../Header/Header'
-import { Menu } from '../Menu/Menu'
-import { Footer } from '../Footer/Footer'
-import styles from './Layout.module.scss'
-import { window, document, exists } from 'browser-monads'
+import { graphql, StaticQuery } from "gatsby"
+import React, { useState } from "react"
+import Headroom from "react-headroom"
+import Helmet from "react-helmet"
+import { Header } from "../Header/Header"
+import { Menu } from "../Menu/Menu"
+import { Footer } from "../Footer/Footer"
+import styles from "./Layout.module.scss"
+import { window, document, exists } from "browser-monads"
 
 export const Layout = ({ children }: any) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const pathname = window.location.pathname
+	const pathname = window.location.pathname
 
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          allSanityMenuItem {
-            nodes {
-              title
-              url
-              id
-            }
-          }
-        }
-      `}
-      render={(data) => (
-        <div className={styles.wrapper}>
-          <div>
-            <Helmet
-              title={'Hugo Carlier'}
-              meta={[
-                {
-                  name: 'description',
-                  content: 'Film Director (BANDITS FR) and Cinematographer.',
-                },
-                { name: 'keywords', content: 'Film Director, Cinematographer' },
-              ]}
-            >
-              <link
-                rel='apple-touch-icon'
-                sizes='180x180'
-                href='/static/apple-touch-icon.png'
-              />
-              <link
-                rel='icon'
-                type='image/png'
-                sizes='32x32'
-                href='/static/favicon-32x32.png'
-              />
-              <link
-                rel='icon'
-                type='image/png'
-                sizes='16x16'
-                href='/static/favicon-16x16.png'
-              />
-              <link rel='manifest' href='/static/site.webmanifest' />
-            </Helmet>
-            <Headroom disableInlineStyles>
-              <Header
-                menuItems={data.allSanityMenuItem}
-                siteTitle={data.site.siteMetadata.title}
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={() => setIsMenuOpen(!isMenuOpen)}
-              />
-            </Headroom>
-            <Menu menuItems={data.allSanityMenuItem} isMenuOpen={isMenuOpen} />
-            <>{children}</>
-          </div>
-          {pathname !== '/contact' ? <Footer /> : null}
-        </div>
-      )}
-    />
-  )
+	return (
+		<StaticQuery
+			query={graphql`
+				query {
+					site {
+						siteMetadata {
+							title
+						}
+					}
+					allSanityMenuItem {
+						nodes {
+							title
+							url
+							id
+						}
+					}
+				}
+			`}
+			render={(data) => (
+				<div className={styles.wrapper}>
+					<div>
+						<Helmet
+							title={"Hugo Carlier"}
+							meta={[
+								{
+									name: "description",
+									content: "cinematographer based in Paris",
+								},
+								{ name: "keywords", content: "Film Director, Cinematographer" },
+							]}
+						>
+							<link
+								rel='apple-touch-icon'
+								sizes='180x180'
+								href='/static/apple-touch-icon.png'
+							/>
+							<link
+								rel='icon'
+								type='image/png'
+								sizes='32x32'
+								href='/static/favicon-32x32.png'
+							/>
+							<link
+								rel='icon'
+								type='image/png'
+								sizes='16x16'
+								href='/static/favicon-16x16.png'
+							/>
+							<link rel='manifest' href='/static/site.webmanifest' />
+						</Helmet>
+						<Headroom disableInlineStyles>
+							<Header
+								menuItems={data.allSanityMenuItem}
+								siteTitle={data.site.siteMetadata.title}
+								isMenuOpen={isMenuOpen}
+								setIsMenuOpen={() => setIsMenuOpen(!isMenuOpen)}
+							/>
+						</Headroom>
+						<Menu menuItems={data.allSanityMenuItem} isMenuOpen={isMenuOpen} />
+						<>{children}</>
+					</div>
+					{pathname !== "/contact" ? <Footer /> : null}
+				</div>
+			)}
+		/>
+	)
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,56 +1,44 @@
-import React, { FunctionComponent } from 'react'
-import { graphql } from 'gatsby'
-import { Layout } from '../components/Layout/Layout'
-import { TextBox } from '../components/TextBox/TextBox'
-import styles from '../styles/pages/contact.module.scss'
+import React, { FunctionComponent } from "react"
+import { graphql } from "gatsby"
+import { Layout } from "../components/Layout/Layout"
+import { TextBox } from "../components/TextBox/TextBox"
+import styles from "../styles/pages/contact.module.scss"
 
 type Props = {
-  props: any
+	props: any
 }
 
 export const query = graphql`
-  {
-    sanityContactPage {
-      _rawContactText
-      email
-      phone
-    }
-  }
+	{
+		sanityContactPage {
+			_rawContactText
+			email
+			phone
+		}
+	}
 `
 
 const Contact: FunctionComponent<Props> = ({ data }: any) => {
-  const { _rawContactText, email, phone } = data.sanityContactPage
+	const { _rawContactText, email, phone } = data.sanityContactPage
 
-  return (
-    <>
-      <Layout>
-        <div className={styles.contactContainer}>
-          <TextBox texts={[_rawContactText[0].children[0].text]} />
-          <section className={styles.contactInfoContainer}>
-            <div className={styles.emailPhoneInsta}>
-              <p>{email}</p>
-              <p>{phone}</p>
-              <a href='https://instagram.com/igotvisions' target='_blank'>
-                Instagram
-              </a>
-            </div>
-            <div className={styles.devLinks}>
-              <p>
-                Made by{' '}
-                <a target='_blank' href='https://github.com/raademar'>
-                  Mattias
-                </a>{' '}
-                &{' '}
-                <a target='_blank' href='https://www.vevang.se/'>
-                  Benjamin
-                </a>
-              </p>
-            </div>
-          </section>
-        </div>
-      </Layout>
-    </>
-  )
+	return (
+		<>
+			<Layout>
+				<div className={styles.contactContainer}>
+					<TextBox texts={[_rawContactText[0].children[0].text]} />
+					<section className={styles.contactInfoContainer}>
+						<div className={styles.emailPhoneInsta}>
+							<p>{email}</p>
+							<p>{phone}</p>
+							<a href='https://instagram.com/igotvisions' target='_blank'>
+								Instagram
+							</a>
+						</div>
+					</section>
+				</div>
+			</Layout>
+		</>
+	)
 }
 
 export default Contact
